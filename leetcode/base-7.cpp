18dd3d045b14a05a5e46c3060d8b6b26eb46cac7
mihaimusteata <mihail.musteata@isa.utm.md>
2025-12-17 21:53:35 +0200

    leetcode: add convert-to-base-7 solution

---
+++ leetcode/base-7.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        int n = abs(num);
+        string ans;
+        while(n)
+        {
+            ans += n % 7 + '0';
+            n /= 7;
+        }
+
+        if(num < 0)
+            ans += '-';
+
+        reverse(ans.begin(), ans.end());
+        return ans.size() ? ans : "0";
+    }
+};
